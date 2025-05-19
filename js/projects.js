2f91c14f9656f693a87e1b20aafbe039f91a377e
shayb319 <50563272+shayb319@users.noreply.github.com>
2025-05-19 11:16:20 -0400

    2025_05_19
    Add some content to the home page

---
+++ js/projects.js
@@ -0,0 +1,34 @@
+const projects = [
+    {
+        id: 4,
+        title: "Project 4",
+        description: "Project 4 description",
+        link: "project.html",
+        image: "images/computer_coffee.jpg",
+        order: 4
+    },
+    {
+        id: 3,
+        title: "Project 3",
+        description: "Project 3 description",
+        link: "project.html",
+        image: "images/code_notebook.jpg",
+        order: 3
+    },
+    {
+        id: 2,
+        title: "Building My Portfolio Site From Scratch",
+        description: "Why I ditched the website builders and dove into HTML instead.",
+        link: "website_article.html",
+        image: "images/website_code.png",
+        order: 2
+    },
+    {
+        id: 1,
+        title: "Best Selling Books",
+        description: "Data visualization project using matplotlib, seaborn and linear regression to analyze sales patterns in best selling books.",
+        link: "best_selling_books.html",
+        image: "images/sales_vs_num_books_graph.png",
+        order: 1
+    }
+];
